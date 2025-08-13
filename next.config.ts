c72f878051472ff895d9c55f100775c8ab990afe
midsane <adityaraj10544@gmail.com>
2025-08-13 18:10:42 +0530

    updated nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ensure the 'output: 'export'' line is NOT present.
-  // It will break image optimization.
 
   images: {
     remotePatterns: [
       {
-        // Pattern for your Supabase storage URL
+        
         protocol: 'https',
         hostname: 'iorrotlayfegnkxhyobm.supabase.co',
         port: '',
         pathname: '**',
       },
       {
-        // Pattern for Google user profile pictures and their subdomains
+      
         protocol: 'https',
         hostname: '*.googleusercontent.com',
         port: '',
